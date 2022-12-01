f6763efe623065ab1b6beadca354e0ba47dda8b9
Aimé Schneider <34367419+aschneiderGit@users.noreply.github.com>
2022-12-01 10:08:23 +0100

    use Link component

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import Link from 'next/link';
+
 function HomePage() {
-	return <h1>The Home Page</h1>;
+	return (
+		<>
+			<h1>The Home Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/nextJS-is-a-great-framework">
+						NextJS is a great Framework
+					</Link>
+				</li>
+				<li> Something-important</li>
+			</ul>
+		</>
+	);
 }
 
 export default HomePage;
